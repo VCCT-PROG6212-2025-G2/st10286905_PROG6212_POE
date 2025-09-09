0913f0fc5a385fb0504114d9b1955c1d33bd83f3
Sky <131005117+skylukemartin@users.noreply.github.com>
2025-09-10 01:33:08 +0200

    Add controllers for each role

---
+++ ContractMonthlyClaimSystem/Controllers/AcademicManagerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMonthlyClaimSystem.Controllers
+{
+    [Authorize(Roles = "AcademicManager")]
+    public class AcademicManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ContractMonthlyClaimSystem/Controllers/LecturerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContractMonthlyClaimSystem.Controllers
+{
+    [Authorize(Roles = "Lecturer")]
+    public class LecturerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
