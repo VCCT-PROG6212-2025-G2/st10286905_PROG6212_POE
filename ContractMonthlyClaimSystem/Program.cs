1babaf6934b19c09ee872a56d02816fbbf34249d
Sky <131005117+skylukemartin@users.noreply.github.com>
2025-09-10 14:45:56 +0200

    Create lecturer helper service

--- ContractMonthlyClaimSystem/Program.cs
+++ ContractMonthlyClaimSystem/Program.cs
@@ -1,5 +1,6 @@
 using ContractMonthlyClaimSystem.Data;
 using ContractMonthlyClaimSystem.Models;
+using ContractMonthlyClaimSystem.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
@@ -27,6 +28,9 @@ internal class Program
 
         // Add database seeder as scoped service
         builder.Services.AddScoped<DatabaseSeeder>();
+
+        // Add helper services
+        builder.Services.AddScoped<LecturerService>();
 
         var app = builder.Build();
 
